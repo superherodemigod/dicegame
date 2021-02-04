a1bccb23318d35fcaa26744643c308560217d3e3
Napoleon <sherlockdev0105@gmail.com>
2021-02-05 01:53:44 +0900

    set background img

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <container className="App-container">
+
+      </container>
     </div>
   );
 }
