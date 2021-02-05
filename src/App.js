70822a22ede1680504588e6206487b1919bd0125
Napoleon <sherlockdev0105@gmail.com>
2021-02-05 23:41:02 +0900

    complete design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, {Component} from 'react'
-import Header from './Header';
-import Jackpot from './Jackpot';
-import Gameboard from './Gameboard';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./Header";
+import Jackpot from "./Jackpot";
+import Gameboard from "./Gameboard";
+import Footer from "./Footer";
+import "./App.css";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="container">
-          <Header />
-          <Jackpot />
+          <div className="headerArea">
+            <Header />
+            <Jackpot />
+          </div>
           <Gameboard />
+          <Footer />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
