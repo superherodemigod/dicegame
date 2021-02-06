1b6372e400d7a230a69020a1a58db4390a4012ff
Napoleon <sherlockdev0105@gmail.com>
2021-02-06 14:18:34 +0900

    complete roll dice function

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import dice1Img from "./imgs/dice1.png";
 import dice2Img from "./imgs/dice2.png";
+import dice3Img from "./imgs/dice3.png";
+import dice4Img from "./imgs/dice4.png";
+import dice5Img from "./imgs/dice5.png";
+import dice6Img from "./imgs/dice6.png";
 import rollBtnImg from "./imgs/roll-btn.png";
 import guideImg from "./imgs/guide.png";
 import paytableImg from "./imgs/paytable.png";
@@ -8,10 +12,37 @@ class Footer extends Component {
   constructor(props) {
     super(props);
     this.rollDice = this.rollDice.bind(this);
+    this.state = {
+      img_path1: dice5Img,
+      alt1: "dice5",
+      img_path2: dice2Img,
+      alt2: "dice2",
+    };
   }
 
   rollDice() {
-    console.log("Roll changed!");
+    const items = [
+      { ItemName: "dice1", ItemImg: dice1Img },
+      { ItemName: "dice2", ItemImg: dice2Img },
+      { ItemName: "dice3", ItemImg: dice3Img },
+      { ItemName: "dice4", ItemImg: dice4Img },
+      { ItemName: "dice5", ItemImg: dice5Img },
+      { ItemName: "dice6", ItemImg: dice6Img },
+    ];
+
+    var item1 = items[Math.floor(Math.random() * items.length)];
+    var item2 = items[Math.floor(Math.random() * items.length)];
+    this.setState({
+      img_path1: item1.ItemImg,
+      alt1: item1.ItemName,
+      img_path2: item2.ItemImg,
+      alt2: item2.ItemName,
+    });
+
+    // const dice1 = Math.floor(Math.random() * 6);
+    // const dice2 = Math.floor(Math.random() * 6);
+    // this.dice1Number = dice1;
+    // console.log("Roll changed!");
   }
   render() {
     return (
@@ -20,8 +51,16 @@ class Footer extends Component {
           <div className="anySevenImg"></div>
         </div>
         <div className="diceArea">
-          <img src={dice1Img} className="dice1Img" alt="" />
-          <img src={dice2Img} className="dice2Img" alt="" />
+          <img
+            src={this.state.img_path1}
+            className="diceImg"
+            alt={this.state.alt1}
+          />
+          <img
+            src={this.state.img_path2}
+            className="diceImg"
+            alt={this.state.alt2}
+          />
         </div>
         <div className="bottomArea">
           <a href="/">
